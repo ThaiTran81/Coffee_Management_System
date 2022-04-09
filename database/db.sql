f57c1af9015a1fb9e861fdd8e66842b721fae2d1
Trần Hoàng Thái <63252276+ThaiTran81@users.noreply.github.com>
2022-04-09 23:50:23 +0700

    Create db.sql

---
+++ database/db.sql
@@ -0,0 +1,154 @@
+CREATE TABLE `account` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` int(11) DEFAULT '0',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `area` (
+  `area_id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`area_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `attendance` (
+  `user_id` int(11) NOT NULL,
+  `check_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`check_time`),
+  CONSTRAINT `fk_attendance_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `bill` (
+  `bill_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `discount` float(255,0) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `payment_type` varchar(255) DEFAULT NULL,
+  `status` int(255) DEFAULT NULL,
+  PRIMARY KEY (`bill_id`),
+  KEY `fk_user_id` (`user_id`),
+  KEY `fk_customer_id` (`customer_id`),
+  CONSTRAINT `fk_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `billDetail` (
+  `billDetail_id` int(11) NOT NULL,
+  `bill_id` int(11) DEFAULT NULL,
+  `itemDetail_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `note` varchar(100) DEFAULT NULL,
+  `discount` float DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`billDetail_id`),
+  KEY `fk_billDetail_bill_idx` (`bill_id`),
+  KEY `fk_billDetail_itemDetail_idx` (`itemDetail_id`),
+  CONSTRAINT `fk_billDetail_bill` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_billDetail_itemDetail` FOREIGN KEY (`itemDetail_id`) REFERENCES `itemDetail` (`itemDetail_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `billPromotion` (
+  `billPromotion_id` int(11) NOT NULL,
+  `promotion_id` int(11) NOT NULL,
+  `discount` float DEFAULT NULL,
+  PRIMARY KEY (`billPromotion_id`),
+  KEY `fk_billPromotion_promotion_idx` (`promotion_id`),
+  CONSTRAINT `fk_billPromotion_promotion` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`promotion_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `point` int(255) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `fk_type_idx` (`type`),
+  CONSTRAINT `fk_type` FOREIGN KEY (`type`) REFERENCES `type` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `itemDetail` (
+  `itemDetail_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` double DEFAULT '0',
+  PRIMARY KEY (`itemDetail_id`),
+  KEY `fk_item_id_idx` (`item_id`),
+  CONSTRAINT `fk_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `itemPromotion` (
+  `itemPromotion_id` int(11) NOT NULL,
+  `promotion_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `discount` float DEFAULT NULL,
+  PRIMARY KEY (`itemPromotion_id`,`promotion_id`,`item_id`),
+  KEY `fk_ itemPromotion_item_idx` (`item_id`),
+  KEY `fk_ itemPromotion_promotion_idx` (`promotion_id`),
+  CONSTRAINT `fk_ itemPromotion_item` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ itemPromotion_promotion` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`promotion_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `promotion` (
+  `promotion_id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`promotion_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `table` (
+  `table_id` int(11) NOT NULL,
+  `area_id` int(11) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `bill_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`table_id`),
+  KEY `fk_table_area_idx` (`area_id`),
+  KEY `fk_table_bill_idx` (`bill_id`),
+  CONSTRAINT `fk_table_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`area_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_table_bill` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`bill_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `type` (
+  `type_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`type_id`),
+  KEY `fk_category_id_idx` (`category_id`),
+  CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(45) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
