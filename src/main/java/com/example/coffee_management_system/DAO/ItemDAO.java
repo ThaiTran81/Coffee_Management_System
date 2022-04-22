9ec905d6f5a5031d682a5f8a4aa2a3b70652a2ec
Trần Hoàng Thái <63252276+ThaiTran81@users.noreply.github.com>
2022-04-22 22:32:22 +0700

    Update ItemDAO.java

--- src/main/java/com/example/coffee_management_system/DAO/ItemDAO.java
+++ src/main/java/com/example/coffee_management_system/DAO/ItemDAO.java
@@ -24,6 +24,7 @@ public class ItemDAO {
             itemDTO.setDescription(rs.getString("description"));
             itemDTO.setCategory(rs.getInt("category"));
             itemDTO.setPrice(rs.getDouble("price"));
+            itemDTO.setImg(rs.getBlob("image"));
 
             lst.add(itemDTO);
         }
@@ -49,6 +50,7 @@ public class ItemDAO {
             itemDTO.setDescription(rs.getString("description"));
             itemDTO.setCategory(rs.getInt("category"));
             itemDTO.setPrice(rs.getDouble("price"));
+            itemDTO.setImg(rs.getBlob("image"));
 
             lst.add(itemDTO);
         }
@@ -74,6 +76,7 @@ public class ItemDAO {
             itemDTO.setDescription(rs.getString("description"));
             itemDTO.setCategory(rs.getInt("category"));
             itemDTO.setPrice(rs.getDouble("price"));
+            itemDTO.setImg(rs.getBlob("image"));
 
             lst.add(itemDTO);
         }
@@ -111,7 +114,35 @@ public class ItemDAO {
         return stmt.executeUpdate();
     }
 
-    public static int insert(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+    public static int update(ItemDTO itemDTO, String img_url) throws SQLException, ClassNotFoundException, FileNotFoundException {
+        if(img_url==null){
+            return update(itemDTO);
+        }
+
+        String sql = "UPDATE `item` " +
+                "SET `name`= ?," +
+                " `category` = ?," +
+                " `description` = ?," +
+                " `price` = ? ,"+
+                " `image`= ? "+
+                "WHERE `item_id` = ?";
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        InputStream in = new FileInputStream(img_url);
+
+        stmt.setString(1, itemDTO.getName());
+        stmt.setInt(2, itemDTO.getCategory());
+        stmt.setString(3, itemDTO.getDescription());
+        stmt.setDouble(4, itemDTO.getPrice());
+        stmt.setBlob(5, in);
+        stmt.setInt(6, itemDTO.getItem_id());
+
+
+        return stmt.executeUpdate();
+    }
+
+    public static int insert(ItemDTO itemDTO) throws SQLException, ClassNotFoundException, FileNotFoundException {
+
         String sql = "INSERT INTO item (name, category, description, price) VALUES(?,?,?,?)";
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement stmt = connection.prepareStatement(sql);
@@ -120,6 +151,26 @@ public class ItemDAO {
         stmt.setInt(2, itemDTO.getCategory());
         stmt.setString(3, itemDTO.getDescription());
         stmt.setDouble(4, itemDTO.getPrice());
+
+        return stmt.executeUpdate();
+    }
+
+    public static int insert(ItemDTO itemDTO, String img_url) throws SQLException, ClassNotFoundException, FileNotFoundException {
+
+        if(img_url==null) {
+            return insert(itemDTO);
+        }
+
+        String sql = "INSERT INTO item (name, category, description, price, image) VALUES(?,?,?,?,?)";
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        InputStream in = new FileInputStream(img_url);
+
+        stmt.setString(1,itemDTO.getName());
+        stmt.setInt(2, itemDTO.getCategory());
+        stmt.setString(3, itemDTO.getDescription());
+        stmt.setDouble(4, itemDTO.getPrice());
+        stmt.setBlob(5, in);
 
         return stmt.executeUpdate();
     }
